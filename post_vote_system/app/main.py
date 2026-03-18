eda958d3f968e851579329d0d28ae3a7f21af6cc
dhunganabimal <dhunganabimal07@gmail.com>
2026-03-18 21:40:23 +0545

    feat:CORS Support Added

--- post_vote_system/app/main.py
+++ post_vote_system/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,APIRouter
+from starlette.middleware.cors import CORSMiddleware
 
 from .database import engine
 from .routers import posts, users, auth, votes
@@ -11,6 +12,13 @@ app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(votes.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"Voting System Api is Running !!!"}
