aac0ca0444b8d3738528d7e93b21d14cb9caa432
Bimal Dhungana <167676907+dhunganabimal@users.noreply.github.com>
2026-03-18 22:20:58 +0545

    Disable redirect slashes in FastAPI app

--- post_vote_system/app/main.py
+++ post_vote_system/app/main.py
@@ -7,7 +7,7 @@ from .routers.votes import vote
 from app import models
 
 models.Base.metadata.create_all(bind=engine)
-app = FastAPI()
+app = FastAPI(redirect_slashes=False)
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
